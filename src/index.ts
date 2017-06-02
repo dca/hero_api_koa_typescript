5495e62967a48681357077424457f0018c0e190f
Dca <dcahsu@gmail.com>
2017-06-02 14:07:27 +0800

    feat: Add base koa2 server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+import * as Koa from 'koa'
+import * as bodyParser from 'koa-bodyparser';
+
+import router from './routes';
+
+const app = new Koa()
+const port = process.env.PORT || 5555;
+
+app.use(bodyParser())
+   .use(router.routes())
+   .use(router.allowedMethods());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+export default app;
